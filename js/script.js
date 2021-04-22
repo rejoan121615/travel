cb8ba88b9fe35f5ee3689251434a27a4289b1646
Mohd Rejoan <wearecrx@gmail.com>
2021-04-23 01:01:01 +0600

    interaction added on just take a look section, pack and go section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-'use strict';
+"use strict";
+
+const GactiveRemover = (list) => {
+    list.forEach((item, index) => {
+        item.classList.remove("active");
+    });
+};
+
+const GactiveAdd = (item) => {
+    item.classList.add("active");
+};
+
+const GimgHandler = (list, item, index) => {
+    item.src = list[index];
+};
+
+// just take a look imgage controller
+const getBtn = document.querySelectorAll(
+    "#takeYourJourney .button_wrapper button"
+);
+const imgStorage = [
+    "./img/takeYourJourney/beach.jpg",
+    "./img/takeYourJourney/hotel.jpg",
+    "./img/takeYourJourney/jungle.jpg",
+];
+const imgContainer = document.querySelector(
+    "#takeYourJourney .figure .figure-img"
+);
+
+console.log(imgContainer);
+
+getBtn.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        GactiveRemover(getBtn);
+        GactiveAdd(item);
+        GimgHandler(imgStorage, imgContainer, index);
+    });
+});
+
+// pack and go section
+
+const packOption = document.querySelectorAll("#packAndGo .pack_wrapper .card");
+
+packOption.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        GactiveRemover(packOption);
+        GactiveAdd(item);
+    });
+});
